1aa1e8c77a6adc7980ab497ccdd43b8fff3f1fd0
Thomas Basche <thomas.basche@wolt.com>
2022-02-08 13:53:31 +0200

    Add some skeleton info

---
+++ chapter1.py
@@ -0,0 +1,6 @@
+"""
+The Boy Scout Rule
+"""
+
+
+# TODO code here about improving as you go. What kind of things can we do? When is it not appropriate?
---
+++ chapter2.py
@@ -0,0 +1,10 @@
+"""
+Meaningful names
+"""
+
+# TODO - pull apart some of the examples in the book and how they can be improved through types
+# When is it ok to use a,b,i,x etc.?
+# noise words - this is still relevant. Take a few minutes to think of the intent of the data. data or information is always tempting - but everything is information! Maybe instead of UserInformation, UserDetails, UserBankAccounts etc.
+# Names that you have to read out
+
+# Rip apart the meaningful context example (side effects, stupid function names)
