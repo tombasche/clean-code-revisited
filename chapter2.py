c7173c41630626a3838ece6cada8e5636e659ef0
Thomas Basche <thomas.basche@wolt.com>
2022-02-08 14:10:50 +0200

    Add insane example of 'good naming'

--- chapter2.py
+++ chapter2.py
@@ -8,3 +8,39 @@ Meaningful names
 # Names that you have to read out
 
 # Rip apart the meaningful context example (side effects, stupid function names)
+
+
+# This is the kind of rubbish the book promotes:
+class GuessStatisticsMessage:
+
+    def __init__(self):
+        self.number = ""
+        self.verb = ""
+        self.plural_modifier = ""
+
+    def make(self, candidate, count):
+        self.create_plural_dependent_message_parts(count)
+        return f"There {self.verb} {self.number} {candidate}{self.plural_modifier} "
+
+    def create_plural_dependent_message_parts(self, count):
+        if count == 0:
+            self.there_are_no_letters()
+        elif count == 1:
+            self.there_is_one_letter()
+        else:
+            self.there_are_many_letters(count)
+
+    def there_are_many_letters(self, count):
+        self.number = str(count)
+        self.verb = "are"
+        self.plural_modifier = "s"
+
+    def there_is_one_letter(self):
+        self.number = "1"
+        self.verb = "is"
+        self.plural_modifier = ""
+
+    def there_are_no_letters(self):
+        self.number = "no"
+        self.verb = "are"
+        self.plural_modifier = "s"
